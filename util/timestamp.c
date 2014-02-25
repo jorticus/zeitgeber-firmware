545b4cb748b8f8f54f10948bddd600a0d2202ab9
Jared Sanson <jared@jared.geek.nz>
2014-02-25 13:23:53 +1300

    Added missing files

---
+++ util/timestamp.c
@@ -0,0 +1,18 @@
+/*
+ * File:   api/timestamp.c
+ * Author: Jared
+ *
+ * Created on 20 February 2014, 1:45 PM
+ */
+
+#include "system.h"
+#include "timestamp.h"
+#include "api/clock.h"
+
+void timestamp2time(timestamp_t ts, rtc_time_t* time) {
+    
+}
+
+void timestamp2date(timestamp_t ts, rtc_date_t* date) {
+
+}
---
+++ util/timestamp.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   api/timestamp.h
+ * Author: Jared
+ *
+ * Created on 20 February 2014, 1:45 PM
+ */
+
+#ifndef TIMESTAMP_H
+#define	TIMESTAMP_H
+
+//typedef uint16 timestamp_t;
+
+#endif	/* TIMESTAMP_H */
+
