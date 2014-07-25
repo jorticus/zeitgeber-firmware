fd8bad150e1abf967a866d275461bb90520ec114
Jared Sanson <jared@jared.geek.nz>
2014-07-26 11:19:46 +1200

    Added kernel diagnostics to error screen
    NOTE: I am not sure if current_task is actually the current task when a Stack error trap occurs.

--- core/error.c
+++ core/error.c
@@ -10,17 +10,21 @@
 #include "system.h"
 #include "core/error.h"
 #include "core/os.h"
+#include "core/kernel.h"
 
 #include "hardware.h"
 #include "api/graphics/gfx.h"
 
 ////////// Code ////////////////////////////////////////////////////////////////
 
+extern task_t* current_task;
+
 static bool in_error = false;
 
 //TODO: What happens with the stack when the stack trap is called???
 
 void CriticalError(const char* msg) {
+    task_t* task = current_task;
 
     // Disable the RTOS
     T1CONbits.TON = 0;
@@ -30,6 +34,11 @@ void CriticalError(const char* msg) {
     ClearImage();
     DrawString("CRITICAL ERROR", 8,8, RED);
     DrawString(msg, 8,18, WHITE);
+    UpdateDisplay();
+
+    // Gather some kernel diagnostics
+    DrawString("Task:", 8,38, WHITE);
+    DrawString(task->name, 45,38, SKYBLUE);
     UpdateDisplay();
 
     while (!_PORT(BTN1) && !_PORT(BTN2) && !_PORT(BTN3) && !_PORT(BTN4));
